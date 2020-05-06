6eac64312dd93545cbff671ce89680a087a138a8
ramasalahat <ramasalahat@gmail.com>
2020-05-06 20:45:57 +0300

    fix

--- server.js
+++ server.js
@@ -1,58 +1,41 @@
 const express = require('express');
-const mongoose=require('mongoose');
-const patient_router=require('./routes/patient');
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const patient_router = require('./routes/patient');
 const Patient = require('./models/patient.model')
 const methodOverride = require('method-override')
-const https=require('https');
+const https = require('https');
 path = require('path');
 
-  
-const app= express();
-	
+
+const app = express();
+
 
 app.use(methodOverride('_method'))
 app.set("view engine", "ejs")
 app.use(express.json());
 
 
-const uri='mongodb+srv://emadarmiti:Th!nker1@dexter-cqfvi.mongodb.net/test?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://emadarmiti:Th!nker1@dexter-cqfvi.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true });
-const connection=mongoose.connection;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
 
-connection.once('open',()=>{
+connection.once('open', () => {
 
     console.log('database is ready');
-    
+
 })
 
-var chemical="acenocoumarol";
 
-app.post('/',  (req, res) => {
- 
-chemical= req.body.chemical;
-    res.redirect('/');
-  
+
+
+app.get('/', async(req, res) => {
+    const patients = await Patient.find().sort({ name: 'asc' })
+    res.render('patient', { patients: patients })
 });
 
 
-app.get('/', async (req, res) => {
- 
-    var url = `https://api.pharmgkb.org/v1/data/chemical?name=${chemical}`;
-    var temp=[];
-    https.get(url,(response)=>{
+app.use('/patient', patient_router);
 
-        console.log(response.statusCode);
-        response.on('data',(data)=>{
-               
-                tmep=JSON.parse(data)['data'][0]['altNames']['trade'];});
-    });
-
-  const patients = await Patient.find().sort({ name: 'asc' })
-  res.render('patient', { patients: patients,temp:temp })
-});
-
-
-app.use('/patient',patient_router);
-
-app.listen(4000,console.log(4000));
+app.listen(4000, console.log(4000));
